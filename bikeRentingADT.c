aae672ccb67398416f9753c42990b403d47f7609
feliventurino <feliventurino@gmail.com>
2023-07-01 17:23:50 -0300

    agregado archivos ADT

--- bikeRentingADT.c
+++ bikeRentingADT.c
@@ -1,5 +1,39 @@
 #include "bikeRentingADT.h"
 
-typedef struct bikeRentingCDT {
-    
+// Renting tipo "New York"
+typedef struct {
+    // Todos los atributos (hay inecesarios)
+    size_t started_at;
+    size_t start_station_id;
+    size_t ended_at;
+} tRentType1;
+
+// Renting tipo "Montreal"
+typedef struct {
+
+} tRentType2;
+
+struct station {
+    string station_name;
+    latitude;
+    longitude
+    id;
+};
+
+// Station tipo "New York"
+typedef struct {
+    struct station stationInfo;
+    //lista
+} tStationType1;
+
+// Station tipo "Montreal"
+typedef struct {
+    struct station stationInfo;
+    //vector
+    tRentType2 * rentInfo;
+} tStationType2;
+
+typedef struct bikeRentingCDT{
+     tStationType1* station1;
+     tStationType2* station2;
 } bikeRentingCDT;
