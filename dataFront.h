4a93c7516d20e71566e401a11fa1dde99b3fe3f0
Uriel Sosa Vazquez <urielsosavazquez@gmail.com>
2023-07-09 16:34:43 -0300

    Cambios minimos.

--- dataFront.h
+++ dataFront.h
@@ -18,25 +18,25 @@
 #include "bikeSharingADT.h"
 
 /*
- * @brief Retorna mensaje de error y termina el programa
+ * @brief   Retorna mensaje de error y termina el programa
  *
- * @param errorStream Origen del error.
- * @param errorText Mensaje de error.
+ * @param   errorStream Origen del error.
+ * @param   errorText Mensaje de error.
  */
 void pError(const char* errorStream, const char* errorText);
 
 /*
- * @brief Valida que los argumentos del programa sean validos.
+ * @brief   Valida que los argumentos del programa sean validos.
  *
- * @param argc Cantidad de argumentos.
- * @param argv Nombre de cada argumento.
+ * @param   argc Cantidad de argumentos.
+ * @param   argv Nombre de cada argumento.
  */
 void validateArguments(int argc, char* argv[]);
 
 void createFiles(FILE* files[], char* argv[], int count, char* flag);
 
 /*
- * @brief Verifica que la carga de datos al ADT sea correcta.
+ * @brief   Verifica que la carga de datos al ADT sea correcta.
  *
  */
 void loadDataToADT(bikeSharingADT bikeSharing, FILE* files[], char* argv);
